8b4e734de5a68379e6b6b2da483c6de0df7f78b2
Marcelo Carvalho <marcelo.scientist@hotmail.com>
2020-09-06 14:42:19 -0300

    The starting Game

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 2*box,
+    y: 2*box
+};
+let direction = "right";
 
-function criarBG() {
+function createBG() {
     context.fillStyle = "lightgreen"
-    context.fillRect(0,0,16 * box,16* box)
+    context.fillRect(0, 0, 16*box, 16*box)
 }
 
-criarBG();
+function createSnake() {
+    for(let i = 0; i < snake.length; i++) {
+        context.fillStyle = "red"
+        context.fillRect(snake[i].x*2, snake[i].y, box, box/2)   
+    }
+}
+
+function startGame() {
+    createBG();
+    createSnake();        
+}
+
+let jogo = setInterval(startGame, 100);
+
+
