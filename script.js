233035c5aa8c1b72e8f06eb22424feae2084fb45
Marcelo Carvalho <marcelo.scientist@hotmail.com>
2020-09-06 14:59:30 -0300

    defining snake moviment

--- script.js
+++ script.js
@@ -3,26 +3,42 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
-    x: 2*box,
-    y: 2*box
+    x: 2 * box,
+    y: 2 * box
 };
 let direction = "right";
 
 function createBG() {
     context.fillStyle = "lightgreen"
-    context.fillRect(0, 0, 16*box, 16*box)
+    context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
 function createSnake() {
-    for(let i = 0; i < snake.length; i++) {
+    for (let i = 0; i < snake.length; i++) {
         context.fillStyle = "red"
-        context.fillRect(snake[i].x*2, snake[i].y, box, box/2)   
+        context.fillRect(snake[i].x * 2, snake[i].y, box, box / 2)
     }
 }
 
 function startGame() {
     createBG();
-    createSnake();        
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction=="right") snakeX += box;
+    if(direction=="left") snakeX -= box;
+    if(direction=="up") snakeY += box;
+    if(direction=="down") snakeY -= box;
+    snake.pop();
+
+    let newHead = {
+        x: sneakX,
+        y: sneakY
+    }
+
+    snake.unshift(newHead)
 }
 
 let jogo = setInterval(startGame, 100);
